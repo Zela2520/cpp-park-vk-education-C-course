d4c4601edcbb9e1a38167758ec00dfcb857efcfb
Daniil Zelenko <daniil-zzz@mail.ru>
2021-10-14 15:05:14 +0200

    fixed case number three

--- project/src/prime.c
+++ project/src/prime.c
@@ -1,10 +1,7 @@
 #include "prime.h"
 
 int is_prime(int number) {
-	if (number == 1 || number == 0) {
-		return 0;
-	}
-	if (number < 0) {
+	if (number < 2) {
 		return 0;
 	}
 	for (int i = 2; i * i <= number; ++i) {
