a294a2ca0fcad0d20b94316bbe2cc4b8d1e809e3
Daniil Zelenko <daniil-zzz@mail.ru>
2021-10-14 15:48:54 +0200

    fixed case number two

--- project/include/utils.h
+++ project/include/utils.h
@@ -2,10 +2,9 @@
 #define PROJECT_INCLUDE_UTILS_H_
 
 #include <stddef.h>
+
 size_t timer_from(int from);
 int custom_pow(int base, int power);
-// TODO(daniil-zzz@mail.ru): Implement `power of` function
-// int custom_pow(int base, int power);
 
 #endif  // PROJECT_INCLUDE_UTILS_H_
 
--- project/src/utils.c
+++ project/src/utils.c
@@ -2,33 +2,33 @@
 #include "utils.h"
 
 size_t timer_from(int from) {
-    size_t counter = 0;
-    for (int i = from; i >= 0; --i) {
-        ++counter;
-	if(i == 0) {
-		printf("%d", i);
-	} else {
-		printf("%d ", i);
-	}
-    }
-    if (counter != 0) {
-    	printf("\n");
-    }
-    return counter;
+	size_t counter = 0;
+    	for (int i = from; i >= 0; --i) {
+        	++counter;
+		if(i == 0) {
+			printf("%d", i);
+		} else {
+			printf("%d ", i);
+		}
+    	}
+    	if (counter != 0) {
+	    	printf("\n");
+    	}
+    	return counter;
 }
 
-// TODO(daniil-zzz@mail.ru): Implement `power of` function
 
 int custom_pow(int base, int power) {
-	int res = 1;
+	int result = 1;
 	int currentPower = base;
 	while (power > 0) {
-	if (power % 2 == 1) {
-		res *= currentPower;
+		// смотрим чему равен последний бит числа в двоичном представлении
+		if (power % 2 == 1) {  // если последний бит равен единицы, то он участвует в двоичном разложение числа
+			result *= currentPower;  // добавим соответствующую степень двойки к результату
+		}
+		currentPower *= currentPower;
+		power = power >> 1;  // переходим к обработке следующего бита
 	}
-	currentPower *= currentPower;
-	power = power >> 1;
-	}
-    return res;
+	return result;
 }
 
