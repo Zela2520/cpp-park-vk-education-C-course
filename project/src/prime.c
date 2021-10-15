3c616ce0e35eb8d86c1b389e7c9d862e1f8224ce
Daniil Zelenko <daniil-zzz@mail.ru>
2021-10-15 19:34:21 +0200

    made the prime function easier

--- project/src/prime.c
+++ project/src/prime.c
@@ -1,10 +1,11 @@
+#include <math.h>
 #include "prime.h"
 
 int is_prime(int number) {
 	if (number < 2) {
 		return 0;
 	}
-	for (int i = 2; i * i <= number; ++i) {
+	for (int i = 2; i <= sqrt(number); ++i) {
 		if (number % i == 0) {
 			return 0;
 		}
