e9273d11dbbbc1710d7cbd3d8d4cf9838a6b2955
Daniil Zelenko <daniil-zzz@mail.ru>
2021-10-25 13:51:21 +0200

    Simplified data input output

--- project/src/streams.c
+++ project/src/streams.c
@@ -8,14 +8,10 @@ int read_from_the_stream(FILE *off_fptr, Data *writing_data, const char *reading
 			strncmp(reading_file, "test.dat", 5) == 0) {
 		clients_information();
 		return fscanf(off_fptr, "%12d%20s%20s%30s%15s%lf%lf%lf",
-				&writing_data->number,
-				writing_data->name,
-				writing_data->surname,
-				writing_data->addres,
-				writing_data->tel_number,
-				&writing_data->indebtedness,
-				&writing_data->credit_limit,
-				&writing_data->cash_payments);
+				&writing_data->number, writing_data->name,
+				writing_data->surname, writing_data->addres,
+				writing_data->tel_number, &writing_data->indebtedness,
+				&writing_data->credit_limit, &writing_data->cash_payments);
 	} else if (strncmp(reading_file, "transaction.dat", 5) == 0) {
 		transactions_information();
 		return fscanf(off_fptr, "%d %lf",
@@ -30,14 +26,10 @@ void write_into_the_stream(FILE *into_fptr, Data reading_data, const char *writi
 			strncmp(writing_file, "blackrecord.dat", 5) == 0 ||
 			strncmp(writing_file, "test.dat", 5) == 0) {
 		fprintf(into_fptr, "%-12d%-11s%-11s%-16s%20s%12.2f%12.2f%12.2f\n",
-				reading_data.number,
-				reading_data.name,
-				reading_data.surname,
-				reading_data.addres,
-				reading_data.tel_number,
-				reading_data.indebtedness,
-				reading_data.credit_limit,
-				reading_data.cash_payments);
+				reading_data.number, reading_data.name,
+				reading_data.surname, reading_data.addres,
+				reading_data.tel_number, reading_data.indebtedness,
+				reading_data.credit_limit, reading_data.cash_payments);
 	} else if (strncmp(writing_file, "transaction.dat", 5) == 0) {
 		fprintf(into_fptr, "%-3d%-6.2f\n", reading_data.number, reading_data.cash_payments);
 	}
