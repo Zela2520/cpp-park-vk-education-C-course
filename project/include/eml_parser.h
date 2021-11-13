5ce872bbda9b3da985a3c8376fdd23ad554061ff
Daniil Zelenko <daniil-zzz@mail.ru>
2021-11-13 17:42:54 +0200

    The eml_parser was implemented

---
+++ project/include/eml_parser.h
@@ -0,0 +1,20 @@
+#ifndef PROJECT_INCLUDE_EML_PARSER_H_
+#define PROJECT_INCLUDE_EML_PARSER_H_
+
+#include <stddef.h>
+#include "object.h"
+
+typedef struct {
+    Object* from;
+    Object* to;
+    Object* date;
+    size_t parts;
+} Result;
+
+Result* create_result(size_t size_from, size_t size_to, size_t size_date);
+Result* get_result(const char* path_to_eml);
+int print(const Result* res);
+int free_result(Result* res);
+
+#endif  // PROJECT_INCLUDE_EML_PARSER_H_
+
